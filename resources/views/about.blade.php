7de2bd5fa4b8cd77c4947c12edbb0d175d224a6d
Arkizan15 <raffibwi38@gmail.com>
2025-06-26 13:50:48 +0700

    laravel-app

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+    <link rel="stylesheet" href="style/style.css">
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <h3>Owner <?= $nama; ?></h3>
+    <h1>ini laman about</h1>
+</body>
+</html>
