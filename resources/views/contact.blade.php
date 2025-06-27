b0337734b51a2b895b7f74f21bc515eef4f647c5
arkanfauzan2008 <raffibwi38@gmail.com>
2025-06-27 20:46:06 +0700

    'dashboard'

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,14 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
+    <title>Contact</title>
 </head>
 <body>
+    <h1>Halaman Contact</h1>
     <a href="/about">about</a>
     <a href="/">home</a> 
     <a href="/contact">contact</a>
     <a href="/blog">blog</a>
-    <h3>Owner <?= $nama; ?></h3>
-    <h1>ini laman about</h1>
+    <ul>
+        <li>
+            <a href="https://instagram.com/arkizan15">Instagram</a>
+        </li>
+    </ul>
+    
 </body>
 </html>
