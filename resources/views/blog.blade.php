4cddc9d511b18863c16134d2b67a509d6e7ab623
arkanfauzan2008 <raffibwi38@gmail.com>
2025-07-02 11:00:56 +0700

    'component'

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-</head>
-<body>
-    <h1>Halaman Blog</h1>
-    <a href="/about">about</a>
-    <a href="/">home</a> 
-    <a href="/contact">contact</a>
-    <a href="/blog">blog</a>
-    <article>
-        <h3>Judul 1</h3>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur expedita iure a. Dignissimos quia saepe natus placeat, sed esse aliquam illum laborum aliquid corrupti modi deserunt voluptatibus alias unde! Reiciendis.
-    </article>
-    
-    <article>
-        <h3>Judul 2</h3>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam iste quae cum corrupti dignissimos porro aliquid repellat? Corrupti quam labore, vel illum eligendi dolor! Modi facere deleniti quisquam? Voluptas, error?
-    </article>
-</body>
-</html>
+<x-layout>
+    <h3 class="text-xl">blalalalal</h3>
+</x-layout>
