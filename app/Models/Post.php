1ee48597391d83e311e61ac9d6922bbd8623c4f6
arkanfauzan2008 <raffibwi38@gmail.com>
2025-07-22 01:34:23 +0700

    'PostFactory'

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model {
+     use HasFactory;
+
     protected $fillable = ['title', 'author', 'slug', 'body'];
    
 }
